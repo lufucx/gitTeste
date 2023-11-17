7d603d4d126ae6f1507e04c0520bbdacea6e750f
luizsilva <luizsilva.miui@gmail.com>
2023-11-17 17:08:32 -0300

    Aula 4

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1>PAGINA INICIAL</h1>
+    <?php
+    $nome = $_POST("nome");
+    $senha = $_POST("senha");
 
-echo "Aula 1!";
-?>
+    if($nome=="Luiz" and $senha== "123")
+     header("Location!: "."site.php");
+    else
+    print("Nome/Senha inválidos");
+
+    ?>
+    
+</body>
+</html>
